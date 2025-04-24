5bbc5cc7520202a227cac37d876f53344d886ba5
bea-trizsiqueira <bia.cortez2@hotmail.com>
2025-04-23 21:02:31 -0300

    Atualização Teste

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+export default function Footer(){
+    return(
+        <>
+         <div>
+            <h1>Ola</h1>
+         </div>
+        </>
+    )
+}
---
+++ src/app/pages/menuTrilhas/page.tsx
@@ -0,0 +1,10 @@
+import Footer from "@/app/components/Footer";
+
+export default function MenuTrilha(){
+    return(
+        <>
+        
+        <Footer />
+        </>
+    )
+}
