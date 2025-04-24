b59a7bba9e9bfd096956c55c20cab42468d820a5
Lupateli <glupateli@gmail.com>
2025-04-24 02:16:44 -0300

    Adição do carrossel

--- src/app/pages/menuTrilhas/page.tsx
+++ src/app/pages/menuTrilhas/page.tsx
@@ -1,30 +1,20 @@
-import NavbarSuperior from "@/app/components/NavbarSuperior";
 import NavbarSuperior2 from "@/app/components/NavbarSuperior";
-import Carousel from "@/app/components/Carousel";
+import Carrossel from "@/app/components/Corrossel"
 
 export default function menuTrilhas(){
-    const fakeData = [
-        { title: "Naruto", image: "https://placehold.co/200x160?text=Naruto" },
-        { title: "One Piece", image: "https://placehold.co/200x160?text=One+Piece" },
-        { title: "Demon Slayer", image: "https://placehold.co/200x160?text=Kimetsu" },
-        { title: "Attack on Titan", image: "https://placehold.co/200x160?text=AOT" },
-        { title: "Jujutsu Kaisen", image: "https://placehold.co/200x160?text=JJK" },
-        ];
+      
     return(
         <>
         <div className="flex min-h-screen flex-col transition-all duration-300 justify-space-between bg-gray-50">
         <NavbarSuperior2 />
-        
-        <div>
- 
-        <div className="bg-white min-h-screen p-6">
-        <h1 className="text-3xl font-bold mb-6">Canal de Trilhas</h1>
-
-        <Carousel title="Continue..." items={fakeData} />
-        <Carousel title="Recomendados para você" items={fakeData} />
-        
+        <div className="pt-3 w-5xl min-w-3xl max-w-5xl mx-auto">
+        <div className="bg-white text-3xl p-4 rounded-xl shadow-md">Continue</div>
         </div>
+        <Carrossel />
+        <div className="pt-3 w-5xl min-w-3xl max-w-5xl mx-auto">
+        <div className="bg-white text-3xl p-4 rounded-xl shadow-md">Novidade</div>
         </div>
+        <Carrossel />
         </div>
     </>
     );
