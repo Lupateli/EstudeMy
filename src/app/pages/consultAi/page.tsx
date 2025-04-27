0508a7c86fe47e4b306d5d68133521d02487561c
João Victor Quaresma de Arruda <joaovictorquaresma2001@gmail.com>
2025-04-27 17:50:03 -0300

    att

--- src/app/pages/consultAi/page.tsx
+++ src/app/pages/consultAi/page.tsx
@@ -1,8 +1,7 @@
 import NavbarSuperior from "@/app/components/NavbarSuperior";
 import MenuLateral from "@/app/components/MenuLateral";
-import Trilha from "@/app/components/Triha";
+import ConsultAi from "@/app/components/ConsultAi";
 import Footer from "@/app/components/Footer";
-
 
 export default function Home() {
     return (
@@ -10,10 +9,9 @@ export default function Home() {
         <div className="flex min-h-screen flex-col transition-all duration-300 justify-space-between-10 bg-gray-50">
             <NavbarSuperior/>
             <MenuLateral/>
-             <Trilha />
-             <Footer/>
-             
-    </div>
-  </> 
+            <ConsultAi />
+            <Footer/>
+        </div>
+    </> 
   );
 }
