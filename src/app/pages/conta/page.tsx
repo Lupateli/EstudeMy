80180cc50595f35fa06871bb2356c6dc31b3e6d4
João Victor Quaresma de Arruda <joaovictorquaresma2001@gmail.com>
2025-05-06 19:40:43 -0300

    att

--- src/app/pages/conta/page.tsx
+++ src/app/pages/conta/page.tsx
@@ -1,15 +1,14 @@
 import Conta from "@/app/components/Conta";
-import NavbarSuperior from "@/app/components/NavbarSuperior";
-import MenuLateral from "@/app/components/MenuLateral";
 import { Nav } from "react-bootstrap";
+import Topo from "@/app/components/Topo";
 
 export default function Home() {
 return (
     <>
         <div className="flex min-h-screen flex-col transition-all duration-300 justify-space-between-10 bg-gray-50">
-            <NavbarSuperior />
+            <Topo />
             <Conta/>
-            <MenuLateral/>
+            
     </div>
   </> 
   );
