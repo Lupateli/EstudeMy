65e8919a57f4db1a61a8629288e94ad8d95c1de2
João Victor Quaresma de Arruda <joaovictorquaresma2001@gmail.com>
2025-06-03 20:13:19 -0300

    Cursos Favoritos
    Adição da página cursos favoritos.

---
+++ src/app/pages/cursosFavoritos/page.tsx
@@ -0,0 +1,18 @@
+import Carrossel from "@/app/components/Carrossel"
+import Footer from "@/app/components/Footer"
+import Topo from "@/app/components/Topo";
+
+export default function meusCursos() {
+  return (
+    <div className="flex min-h-screen flex-col transition-all duration-300 justify-space-between bg-gray-50">
+      <Topo />
+      <div className="flex flex-row w-full h-full">
+        <div className="flex-grow pt-3 w-5xl min-w-3xl max-w-5xl mx-auto">
+          <div className="text-3xl p-4 rounded-xl al">Meus Cursos</div>
+          <Carrossel />
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
