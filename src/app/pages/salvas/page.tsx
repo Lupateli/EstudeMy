4452120da8071043bac4fe55329c5eb90e6a0bbd
milysj <joaomilone@hotmail.com>
2025-06-16 10:05:24 -0300

    att pagina salvas

--- src/app/pages/salvas/page.tsx
+++ src/app/pages/salvas/page.tsx
@@ -2,18 +2,19 @@ import React from "react";
 import Footer from "@/app/components/Footer";
 
 import Topo from "@/app/components/Topo";
+import Carrousel from "@/app/components/Carrousel";
 
 export default function PaginaLicoesSalvas() {
   return (
-    <div className="flex min-h-screen flex-col transition-all duration-300 justify-space-between bg-gray-50">
-      <Topo />
-      <div className="flex flex-row w-full h-full">
-        <div className="flex-grow pt-3 w-5xl min-w-3xl max-w-5xl mx-auto">
-          <div className="text-3xl p-4 rounded-xl">Lições Salvas</div>
-       carrosel
+    <>
+      <div className="flex min-h-screen flex-col transition-all duration-300 justify-space-between bg-gray-50">
+        <Topo />
+        <div className="pt-3 w-full max-w-6xl mx-auto px-4">
+          <div className=" text-3xl p-4 rounded-xl al">Lições Salvas</div>
         </div>
+        <Carrousel />
       </div>
       <Footer />
-    </div>
+    </>
   );
 }
