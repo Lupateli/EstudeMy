5b2e5337438a24358ecb2f513841da2c0684f4c3
milysj <joaomilone@hotmail.com>
2025-06-16 10:45:53 -0300

    melhoria de responsividade das paginas

--- src/app/pages/perfil/page.tsx
+++ src/app/pages/perfil/page.tsx
@@ -7,8 +7,8 @@ export default function PerfilPage() {
   return (
     <div className="flex flex-col min-h-screen bg-gray-100">
       <Topo />
-      <main className="main mx-20 p-5">
-        <div className="p-4">
+      <div className="pt-3 w-full max-w-6xl mx-auto px-4">
+          <div className=" text-3xl p-4 rounded-xl al">
           <img className="mx-auto"
             src="/img/personagem.png"
             alt="Imagem do personagem Guerreiro"
@@ -28,7 +28,7 @@ export default function PerfilPage() {
           </Link>
           <Link  className="blue-btn" href={'/pages/conquistas'}>CONQUISTAS</Link>
         </div>
-      </main>
+      </div>
       <Footer />
     </div>
   );
--- src/app/pages/ranking/page.tsx
+++ src/app/pages/ranking/page.tsx
@@ -7,12 +7,11 @@ export default function RankingPage() {
   return (
     <div className="flex min-h-screen flex-col transition-all duration-300 justify-space-between bg-gray-50">
       <Topo />
-      <div className="flex flex-row w-full h-full">
-        <div className="flex-grow pt-3 w-5xl min-w-3xl max-w-5xl mx-auto">
+      <div className="pt-3 w-full max-w-6xl mx-auto px-4">
+        
           <div className="text-3xl p-4 rounded-xl">Ranking</div>
           <Ranking />
-          
-        </div>
+        
       </div>
       <Footer />
     </div>
